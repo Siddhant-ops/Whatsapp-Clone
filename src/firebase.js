70bc3a65ee00a1c06f4eff3203b8de6cf02700d9
Siddhant-ops <siddhantdalvi3@gmail.com>
2020-12-04 16:55:41 +0530

    updated

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,5 @@
 import firebase from "firebase";
-import firebaseConfig from "./firebaseConfig";
+import firebaseConfig from "./FirebaseConfig";
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
