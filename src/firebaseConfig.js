9b2b7d825aa606b2507552066ff60bb944b93ab2
Siddhant-ops <siddhantdalvi3@gmail.com>
2020-12-04 16:30:37 +0530

    updated

---
+++ src/firebaseConfig.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC8KNbtyWu-Xqkaz3ZuQOOUP7nL-jo1SXM",
+  authDomain: "whatsapp-d3cf6.firebaseapp.com",
+  databaseURL: "https://whatsapp-d3cf6.firebaseio.com",
+  projectId: "whatsapp-d3cf6",
+  storageBucket: "whatsapp-d3cf6.appspot.com",
+  messagingSenderId: "169007254408",
+  appId: "1:169007254408:web:6ba3f5d216cb626c34483d",
+  measurementId: "G-7XHVJW0QFB",
+};
+
+export default firebaseConfig;
